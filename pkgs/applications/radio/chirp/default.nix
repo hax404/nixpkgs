5f6a61147f464f03e06e0aefdb113ffa90490297
Georg Haas <hax404foogit@hax404.de>
2020-01-05 13:40:42 +0100

    chirp: first patches to get it working with python3

--- pkgs/applications/radio/chirp/default.nix
+++ pkgs/applications/radio/chirp/default.nix
@@ -1,18 +1,34 @@
 { stdenv
-, fetchurl
-, python2
+, fetchhg
+, gettext
+, python3
 }:
-python2.pkgs.buildPythonApplication rec {
-  pname = "chirp-daily";
+python3.pkgs.buildPythonApplication rec {
+  pname = "chirp";
   version = "20191221";
 
-  src = fetchurl {
-    url = "https://trac.chirp.danplanet.com/chirp_daily/daily-${version}/${pname}-${version}.tar.gz";
-    sha256 = "1f4h45cbaq3rssl95xax8gn2bm1slnsbgds479db46czgq6y1qhy";
+  src = fetchhg {
+    url = "http://d-rats.com/hg/chirp.hg";
+    rev = "py3";
+    sha256 = "08ynxf21v3f6f7pz18znxspgwhkwjjljxx2d1f3xhwcyr0nxalfr";
+
   };
 
-  propagatedBuildInputs = with python2.pkgs; [
-    pygtk pyserial libxml2 future
+  patches = [
+    ./patches/locale-makefile.patch
+    ./patches/chirp-ui-mainapp.py.patch
+    ./patches/chirp-drivers-kguv8dplus.py.patch
+    ./patches/chirp-drivers-ftm350.py.patch
+  ];
+
+    #with python3.pkgs; [
+    #python37Packages.
+  propagatedBuildInputs = [
+    python3.pkgs.pyserial
+    python3.pkgs.libxml2
+    python3.pkgs.future
+    python3.pkgs.six
+    gettext
   ];
 
   meta = with stdenv.lib; {
